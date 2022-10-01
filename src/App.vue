51bd9f167fc821f80d1ffdc7dda5216dea680a9b
Alicia Barrios <aliciabgo@gmail.com>
2022-10-01 16:01:40 +0200

    iteration 1 done

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,49 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+  import contacts from './contacts.json';
+  import { reactive } from 'vue';
+
+  const fiveFirstContacts = reactive(contacts.slice(0,5));
+
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <h1>IronContacts</h1>
+  <table>
+    <thead>
+      <tr>
+        <th>Picture</th>
+        <th>Name</th>
+        <th>Popularity</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="contact in fiveFirstContacts" :key="contact.id">
+        <td> <img :src="contact.pictureUrl" /></td>
+        <td> {{ contact.name }} </td>
+        <td> {{ contact.popularity}} </td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+
+  table {
+    margin: auto;
+  }
+  
+  img {
+    width: 70px;
+    height: auto;
+  }
 </style>
