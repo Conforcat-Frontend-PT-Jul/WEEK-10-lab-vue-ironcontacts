756219081378625bb32e214a5fae24d91cfdb9c4
alelof <alejandra.lof@gmail.com>
2022-10-03 22:35:14 +0200

    done

--- src/App.vue
+++ src/App.vue
@@ -1,77 +1,70 @@
 <script setup>
-//import HelloWorld from './components/HelloWorld.vue'
 import { reactive } from 'vue'
 import contacts from "./contacts.json";
 
-let currentContact = reactive(
-  {
-    "name": "",
-    "pictureUrl": "",
-    "popularity": "",
-    "id": "",
-    "wonOscar": "",
-    "wonEmmy": ""
-  }
-
-);
+let contactList = reactive([]);
+for (let i = 0; i < 5; i++) {
+  contactList.push(contacts[i]);
+}
 
 function add() {
-  const min = 5;
-  const max = contacts.length - 1;
+  let min = contactList.length;  /* Min will change when I delete/add contacts from displayed list */
+  const max = contacts.length - 1; /* Max won't change unless more items added in the json file */
   const toAdd = Math.floor(Math.random() * (max - min + 1) + min);
-  console.log("Add Clicked!!!", max);
-  console.log(toAdd);
-  currentContact = {
-    "name": contacts[toAdd].name,
-    "pictureUrl": contacts[toAdd].pictureUrl,
-    "popularity": contacts[toAdd].popularity,
-    "id": contacts[toAdd].id,
-    "wonOscar": contacts[toAdd].wonOscar,
-    "wonEmmy": contacts[toAdd].wonEmmy
-  }
+  contactList.push(contacts[toAdd]);
+}
 
-  console.log("Random:\n", currentContact);
+function remove(element) {
+  const index = contactList.findIndex(object => {
+    return object.id === element;
+  });
+  this.contactList.splice(index, 1);
+}
+
+function sortByName(){
+  contactList.sort((a, b) => a.name.localeCompare(b.name));
+}
+
+function sortByPopularity(){
+  contactList.sort((a, b) => (b.popularity - a.popularity));
 }
 </script>
 
 <template>
-  <!-- <HelloWorld /> -->
   <h1>IronContacts</h1>
   <div>
     <button @click="add()">Add random</button>
+    <button @click="sortByName()">Sort by Name</button>
+    <button @click="sortByPopularity()">Sort by Popularity</button>
   </div>
   <table>
-    <tr>
-      <th>Pcture</th>
-      <th>Name</th>
-      <th>Popularity</th>
-      <th>Won Oscar</th>
-      <th>Won Emmy</th>
-    </tr>
+    <thead>
+      <tr>
+        <th>Picture</th>
+        <th>Name</th>
+        <th>Popularity</th>
+        <th>Won Oscar</th>
+        <th>Won Emmy</th>
+        <th>Actions</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="contact in contactList" :key="contact.id">
+        <td><img :src="contact.pictureUrl"></td>
+        <td>{{contact.name}}</td>
+        <td>{{contact.popularity.toFixed(2)}}</td>
+        <td v-if="contact.wonOscar">
+          <img class="icon" src="./assets/oscar.png">
+        </td>
+        <td v-else></td>
+        <td v-if="contact.wonEmmy">
+          <img class="icon" src="./assets/emy.png">
+        </td>
+        <td v-else></td>
+        <td class="removeBtn"><img class="icon" @click="remove(contact.id)" src="./assets/bin.png"></td>
+      </tr>
+    </tbody>
 
-    <tr v-for="index in 5" :key="index">
-      <td><img :src="contacts[index-1].pictureUrl"></td>
-      <td :name="name">{{contacts[index-1].name}}</td>
-      <td>{{contacts[index-1].popularity}}</td>
-      <td v-if="contacts[index-1].wonOscar">
-        <img class="icon" src="./assets/oscar.png">
-      </td>
-      <td v-else></td>
-      <td v-if="contacts[index-1].wonEmmy">
-        <img class="icon" src="./assets/emy.png">
-      </td>
-      <td v-else></td>
-    </tr>
-
-
-    <!--
-    <tr v-for="contact in contacts" :key="contact.id">
-      <td>{{contact.pictureUrl}}</td>
-      <td>{{contact.name}}</td>
-      <td>{{contact.popularity}}</td>
-      <td>{{contact.wonOscar}}</td>
-      <td>{{contact.wonEmmy}}</td>
-    </tr>  -->
   </table>
 </template>
 
@@ -82,7 +75,7 @@ function add() {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 10px;
 }
 
 div {
@@ -90,7 +83,7 @@ div {
 }
 
 img {
-  height: 40px;
+  height: 80px;
 }
 
 table {
@@ -115,7 +108,7 @@ tr:hover {
 th {
   padding-top: 12px;
   padding-bottom: 12px;
-  text-align: left;
+  text-align: center;
   background-color: #0489aa;
   color: white;
 }
@@ -124,5 +117,34 @@ th {
   width: 30px;
   height: 30px;
 
+}
+
+.removeBtn {
+  margin: 0;
+  cursor: pointer;
+}
+
+.removeBtn:hover {
+  background-color: rgb(255, 96, 96);
+}
+
+button {
+  background-color: #04aa78; /* Green */
+  border: none;
+  border-radius: 6px;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  font-weight: 700;
+  display: inline-block;
+  font-size: 18px;
+  margin: 4px 10px;  
+  cursor: pointer;
+  transition-duration: 0.4s;
+}
+
+button:hover {
+  box-shadow: 0 10px 12px 0 rgba(0,0,0,0.24),0 17px 50px 0 rgba(0,0,0,0.19);
+  background-color: #0436aa; /* Green */
 }
 </style>
