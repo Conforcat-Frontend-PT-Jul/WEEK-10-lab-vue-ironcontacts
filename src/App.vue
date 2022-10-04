0d2cacfd4eb2dbe755dac2599eed85c70fedfe90
Mihhedkol <mihhdekol@gmail.com>
2022-10-04 20:16:56 +0200

    iteration1

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,32 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import contacts from "./contacts.json";
+
+import { ref } from "vue";
+
+const fiveContacts = ref(contacts.slice(0, 5));
+
+console.log(fiveContacts);
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div id="app">
+    <table>
+      <tr>
+        <th>Picture</th>
+        <th>Name</th>
+        <th>Popularity</th>
+      </tr>
+      <tr v-for="contact in fiveContacts">
+        <td><img :src="contact.pictureUrl" /></td>
+        <td>{{ contact.name }}</td>
+        <td>{{ contact.popularity }}</td>
+      </tr>
+    </table>
+  </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+img {
+  width: 80px;
 }
 </style>
