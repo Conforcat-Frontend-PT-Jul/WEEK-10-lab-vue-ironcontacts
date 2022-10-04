3c8ead568b8a5cf164ea6896c9f3a31955fdb0ec
Lollo Windows <lorenzocorio@duck.com>
2022-10-04 22:01:07 +0200

    Add iteration 4

--- src/App.vue
+++ src/App.vue
@@ -16,13 +16,31 @@ function addRandomContact() {
   contacts.value.push(allContacts[randomItem]);
 }
 
+function sortContacts(attr) {
+  console.log(contacts.value);
+  if(attr === 'Popularity') {
+    contacts.value.sort( (a,b) => b.popularity - a.popularity )
+  } else if (attr === 'Name') {
+    contacts.value.sort( (x,y) => {  
+          let a = x.name.toUpperCase(),
+              b = y.name.toUpperCase();
+          return a == b ? 0 : a > b ? 1 : -1; 
+    })
+  }
+  console.log(contacts.value);
+}
+
 </script>
 
 <template>
   <header>
     <h1>IronContacts</h1>
   </header>
-  <button @click="addRandomContact">Add random contact</button>
+  <div>
+    <button @click="addRandomContact">Add random contact</button>
+    <button @click="sortContacts('Popularity')">Sort by popularity</button>
+    <button @click="sortContacts('Name')">Sort by name</button>
+  </div>
   <br>
   <table>
     <tr>
