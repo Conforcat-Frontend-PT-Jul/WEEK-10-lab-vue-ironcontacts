b2c0af802b5d3286e32ff799c8126ace57e6a63e
Mihhedkol <mihhdekol@gmail.com>
2022-10-04 21:44:29 +0200

    iteration4

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,39 @@ import contacts from "./contacts.json";
 
 import { ref } from "vue";
 
-const fiveContacts = ref(contacts.slice(0, 10));
+let fiveContacts = ref(contacts.slice(0, 5));
 
 console.log(fiveContacts);
+
+function addRandomContact() {
+  let randomContact = contacts[Math.floor(Math.random() * contacts.length)];
+
+  if (fiveContacts.value.includes(randomContact)) {
+    return;
+  } else {
+    fiveContacts.value.push(randomContact);
+  }
+}
+
+function sortByPopularity() {
+  fiveContacts.value.sort((a, b) => b.popularity - a.popularity);
+  return fiveContacts;
+}
+
+function sortByName() {
+  fiveContacts.value.sort((a, b) => a.name.localeCompare(b.name));
+  return fiveContacts;
+}
 </script>
 
 <template>
   <div id="app">
+    <div id="header">
+      <p>Iron Contacts</p>
+      <button @click="addRandomContact()">Add Random Contact</button>
+      <button @click="sortByPopularity()">Sort by popularity</button>
+      <button @click="sortByName()">Sort by name</button>
+    </div>
     <table>
       <tr>
         <th>Picture</th>
@@ -21,7 +47,7 @@ console.log(fiveContacts);
       <tr v-for="contact in fiveContacts">
         <td><img :src="contact.pictureUrl" /></td>
         <td>{{ contact.name }}</td>
-        <td>{{ contact.popularity }}</td>
+        <td>{{ contact.popularity.toFixed(2) }}</td>
         <td v-if="contact.wonOscar">🏆</td>
         <td v-else="contact.wonOscar"></td>
         <td v-if="contact.wonEmmy">🏆</td>
@@ -32,6 +58,11 @@ console.log(fiveContacts);
 </template>
 
 <style>
+p {
+  font-size: 2rem;
+  font-weight: 700;
+}
+
 img {
   width: 80px;
 }
@@ -40,5 +71,11 @@ table {
   text-align: center;
   border-spacing: 20px;
   border-spacing: 20px;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+#header {
+  text-align: center;
 }
 </style>
