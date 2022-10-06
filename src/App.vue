eb69fa4617fc0773c2dc77b381539431446648c3
claudiamurall <claudiamurall@gmail.com>
2022-10-06 21:44:09 +0200

    Iteration 2 done

--- src/App.vue
+++ src/App.vue
@@ -8,11 +8,19 @@
         <th>Picture</th>
         <th>Name</th>
         <th>Popularity</th>
+        <th>Won an Oscar</th>
+        <th>Won an Emmy</th>
       </tr>
       <tr v-for="contact in fiveContacts">
         <td><img :src="contact.pictureUrl"></td>
         <td>{{ contact.name }}</td>
         <td>{{ contact.popularity.toFixed(2) }}</td>
+        <td v-if="contact.wonOscar">🏆</td>
+        <td v-else="contact.wonOscar"></td>
+        <td v-if="contact.wonEmmy">🏆</td>
+        <td v-else="contact.wonEmmy"></td>
+
+        <td></td>
       </tr>
   </table>
 </div>
@@ -24,7 +32,7 @@
   import contacts from './contacts.json'
   import { ref } from 'vue';
 
-  const fiveContacts = ref(contacts.slice(0, 5)) 
+  const fiveContacts = ref(contacts.slice(0, 20)) 
 
 </script>
 
