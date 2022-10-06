367f9f7e37de83f0f8e36c9183fb04b1e6044a45
claudiamurall <claudiamurall@gmail.com>
2022-10-06 21:25:04 +0200

    Iteration 1 done

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,47 @@
-import "./App.css";
+<template>
+  <div class="app">
+  </div>
+  <div>
+    <h1>IronContacts</h1>
+  <table class="table">
+      <tr>
+        <th>Picture</th>
+        <th>Name</th>
+        <th>Popularity</th>
+      </tr>
+      <tr v-for="contact in fiveContacts">
+        <td><img :src="contact.pictureUrl"></td>
+        <td>{{ contact.name }}</td>
+        <td>{{ contact.popularity.toFixed(2) }}</td>
+      </tr>
+  </table>
+</div>
 
-function App() {
-  <template>
-  <div id="app"></div>
+
 </template>
+
+<script setup>
+  import contacts from './contacts.json'
+  import { ref } from 'vue';
+
+  const fiveContacts = ref(contacts.slice(0, 5)) 
+
+</script>
+
+<style>
+
+h1 {
+  text-align: center;
 }
-export default App;
+
+img {
+  width: 80px;
+}
+
+.table {
+  text-align: center;
+  margin: 30px auto 30px auto;
+
+}
+
+</style>
