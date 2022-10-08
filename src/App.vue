5992e8627eaea4d2bd3c97e2925b65381061a170
claudiamurall <claudiamurall@gmail.com>
2022-10-08 11:07:02 +0200

    done

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="app">
   </div>
-  <div>
+  <header>
+    <div class="ironContacts">
     <h1>IronContacts</h1>
+    <div>
+    <button @click="addRandomContact" >Add Random Contact</button>
+    <button @click="sortPopularity" >Sort By Popularity</button>
+    <button @click="sortName" >Sort By Name</button>
+    </div>
+    </div>
+  </header>
   <table class="table">
       <tr>
         <th>Picture</th>
@@ -19,11 +27,11 @@
         <td v-else="contact.wonOscar"></td>
         <td v-if="contact.wonEmmy">🏆</td>
         <td v-else="contact.wonEmmy"></td>
-
+        <button @click="deleteContact(contact.id)">Delete</button>
         <td></td>
       </tr>
   </table>
-</div>
+
 
 
 </template>
@@ -32,7 +40,37 @@
   import contacts from './contacts.json'
   import { ref } from 'vue';
 
-  const fiveContacts = ref(contacts.slice(0, 20)) 
+  const fiveContacts = ref(contacts.slice(0, ));
+
+  function addRandomContact() {
+  let randomContact = contacts[Math.floor(Math.random() * contacts.length)];
+  if (fiveContacts.value.includes(randomContact)) {
+    return;
+  } else {
+    fiveContacts.value.push(randomContact);
+  }
+}
+
+  function sortPopularity(){
+  fiveContacts.value.sort((a, b) => b.popularity - a.popularity);
+  return fiveContacts;
+
+  }
+
+  function sortName(){
+   fiveContacts.value.sort((a, b) => a.name.localeCompare(b.name));
+  return fiveContacts;
+  }
+
+  function deleteContact(id) {
+  fiveContacts.value.forEach((contact, index) => {
+       if (contact.id === id) {
+      fiveContacts.value.splice(index, 1);
+     
+      return fiveContacts;
+    }
+  });
+}
 
 </script>
 
@@ -51,5 +89,8 @@ img {
   margin: 30px auto 30px auto;
 
 }
+.ironContacts {
+text-align: center;
 
+}
 </style>
